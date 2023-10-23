77262a7b887f79e11f0f4c340c3d9a1fc5e8af6b
InedibleKirby <drewstillman13@gmail.com>
2023-10-23 18:18:35 -0500

    Update setupscript.sh steamgames

--- setupscript.sh
+++ setupscript.sh
@@ -6,11 +6,11 @@ ls
 #sed -i 's|	//"cstrike"		"C:\steamcmd\steamapps\common\Counter-Strike Source Dedicated Server\cstrike"|"cstrike" "/steamgames/css/cstrike"' /steamgames/gmod/garrysmod/cfg/mount.cfg
 
 #Create server launch script file
-#touch /data/gmod/sandboxstart.sh
-#echo "/data/gmod/srcds_run -console -game garrysmod -tickrate 100 -port 27015 +gamemode sandbox -maxplayers 20" > /steamgames/gmod/sandboxstart.sh
+#touch /steamgames/gmod/sandboxstart.sh
+#echo "/steamgames/gmod/srcds_run -console -game garrysmod -tickrate 100 -port 27015 +gamemode sandbox -maxplayers 20" > /steamgames/gmod/sandboxstart.sh
 
-#echo "/data/gmod/srcds_run -console -game garrysmod -tickrate 100 -port 27015 +gamemode terrortown -maxplayers 20 +host_workshop_collection 681278863" > /data/gmod/tttstart.sh
+#echo "/steamgames/gmod/srcds_run -console -game garrysmod -tickrate 100 -port 27015 +gamemode terrortown -maxplayers 20 +host_workshop_collection 681278863" > /steamgames/gmod/tttstart.sh
 
 #Kickoff launch script file
-#source /data/gmod/sandboxstart.sh
-#source /data/gmod/tttstart.sh
+#source /steamgames/gmod/sandboxstart.sh
+#source /steamgames/gmod/tttstart.sh
