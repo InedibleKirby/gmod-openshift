e8aab5b4bbc2c16aba9546866682e4b8537f55ae
InedibleKirby <drewstillman13@gmail.com>
2023-10-23 18:55:39 -0500

    Update setupscript.sh whoops target locally

--- setupscript.sh
+++ setupscript.sh
@@ -29,7 +29,7 @@ chmod +777 srcds_linux
 chmod +777 sandboxstart.sh
 
 #Create server launch script file
-srcds_run -console -game garrysmod -tickrate 100 -port 27015 +gamemode sandbox -maxplayers 20 +map gm_construct
+./srcds_run -console -game garrysmod -tickrate 100 -port 27015 +gamemode sandbox -maxplayers 20 +map gm_construct
 
 #echo "/steamgames/gmod/srcds_run -console -game garrysmod -tickrate 100 -port 27015 +gamemode terrortown -maxplayers 20 +host_workshop_collection 681278863" > /steamgames/gmod/tttstart.sh
 
