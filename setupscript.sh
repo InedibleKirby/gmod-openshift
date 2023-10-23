2ae3a1be745fca62e5872be8bad57b946591f307
InedibleKirby <drewstillman13@gmail.com>
2023-10-23 17:49:40 -0500

    Update setupscript.sh

--- setupscript.sh
+++ setupscript.sh
@@ -1,1 +1,2 @@
-
+#!/bin/bash
+sed -i 's|"cstrike"	"C:\steamcmd\steamapps\common\Counter-Strike Source Dedicated Server\cstrike"|"cstrike" "/steamgames/css/cstrike" /steamgames/gmod/garrysmod/cfg/mount.cfg'
