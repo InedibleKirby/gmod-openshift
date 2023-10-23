50bb1bdbc278421c97f92a9b30cb167762f73874
InedibleKirby <drewstillman13@gmail.com>
2023-10-23 18:05:27 -0500

    Update setupscript.sh try to fix sed

--- setupscript.sh
+++ setupscript.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 #Find and replace CSS mounting config
-sed -i 's|"cstrike"	"C:\steamcmd\steamapps\common\Counter-Strike Source Dedicated Server\cstrike"|"cstrike" "/data/css/cstrike"' /data/gmod/garrysmod/cfg/mount.cfg
+sed -i 's|	//"cstrike"		"C:\steamcmd\steamapps\common\Counter-Strike Source Dedicated Server\cstrike"|"cstrike" "/data/css/cstrike"' /data/gmod/garrysmod/cfg/mount.cfg
 
 #Create server launch script file
+touch /data/gmod/sandboxstart.sh
 echo "/data/gmod/srcds_run -console -game garrysmod -tickrate 100 -port 27015 +gamemode sandbox -maxplayers 20" > /data/gmod/sandboxstart.sh
 
 #echo "/data/gmod/srcds_run -console -game garrysmod -tickrate 100 -port 27015 +gamemode terrortown -maxplayers 20 +host_workshop_collection 681278863" > /data/gmod/tttstart.sh
