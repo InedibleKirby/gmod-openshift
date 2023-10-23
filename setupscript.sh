0236cae336bd601a6c99f872834f09f3dcedaa92
InedibleKirby <drewstillman13@gmail.com>
2023-10-23 18:28:10 -0500

    Update setupscript.sh cat EOF stuff

--- setupscript.sh
+++ setupscript.sh
@@ -1,16 +1,33 @@
 #!/bin/bash
-cd steamgames
-ls
 
-#Find and replace CSS mounting config
-#sed -i 's|	//"cstrike"		"C:\steamcmd\steamapps\common\Counter-Strike Source Dedicated Server\cstrike"|"cstrike" "/steamgames/css/cstrike"' /steamgames/gmod/garrysmod/cfg/mount.cfg
+#Replace mounting config
+cat << EOF > /steamgames/gmod/garrysmod/cfg/mount.cfg
+"mountcfg"
+{
+	"cstrike"	"/steamgames/css/cstrike"
+} 
+EOF
+
+cat << EOF > /steamgames/gmod/garrysmod/cfg/mountdepots.txt
+"gamedepotsystem"
+{
+	"cstrike"	"1"
+	"hl1"		"0"
+	"hl1_hd"	"0"
+	"hl2"		"0"
+	"hl2mp"		"0"
+	"episodic"	"0"
+	"ep2"		"0"
+	"lostcoast"	"0"
+}
+EOF
 
 #Create server launch script file
-#touch /steamgames/gmod/sandboxstart.sh
-#echo "/steamgames/gmod/srcds_run -console -game garrysmod -tickrate 100 -port 27015 +gamemode sandbox -maxplayers 20" > /steamgames/gmod/sandboxstart.sh
+touch /steamgames/gmod/sandboxstart.sh
+echo "/steamgames/gmod/srcds_run -console -game garrysmod -tickrate 100 -port 27015 +gamemode sandbox -maxplayers 20" > /steamgames/gmod/sandboxstart.sh
 
 #echo "/steamgames/gmod/srcds_run -console -game garrysmod -tickrate 100 -port 27015 +gamemode terrortown -maxplayers 20 +host_workshop_collection 681278863" > /steamgames/gmod/tttstart.sh
 
 #Kickoff launch script file
-#source /steamgames/gmod/sandboxstart.sh
+source /steamgames/gmod/sandboxstart.sh
 #source /steamgames/gmod/tttstart.sh
