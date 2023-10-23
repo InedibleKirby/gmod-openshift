f5bcc15b40bb399585b90181389e4566b48b6f9a
InedibleKirby <drewstillman13@gmail.com>
2023-10-23 17:50:25 -0500

    Update setupscript.sh

--- setupscript.sh
+++ setupscript.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
 sed -i 's|"cstrike"	"C:\steamcmd\steamapps\common\Counter-Strike Source Dedicated Server\cstrike"|"cstrike" "/steamgames/css/cstrike" /steamgames/gmod/garrysmod/cfg/mount.cfg'
+
+
+#echo "/steamgames/gmod/srcds_run -console -game garrysmod -tickrate 100 -port 27015 +gamemode terrortown -maxplayers 20 +host_workshop_collection 681278863" > /steamgames/gmod/tttstart.sh
+
+/steamgames/gmod/sandboxstart.sh
