a3be30402d5b22cc63ad0efb9b79f9b3fcb0c3c9
InedibleKirby <drewstillman13@gmail.com>
2023-10-23 20:46:50 -0500

    Update setupscript.sh -y end me

--- setupscript.sh
+++ setupscript.sh
@@ -2,7 +2,7 @@
 
 dpkg --add-architecture i386
 apt update
-apt-get install libstdc++6:i386
+apt-get install libstdc++6:i386 -y
 
 #Replace mounting config
 cat << EOF > /steamgames/gmod/garrysmod/cfg/mount.cfg
